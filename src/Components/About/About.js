91662235dc196eca07310bf744e4d550722db20d
Medvezo <mansur.mustafayev@gmail.com>
2023-05-30 01:43:46 +0400

    Ready About Section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {
-	CloudArrowUpIcon,
-	LockClosedIcon,
-	ServerIcon,
+	MapIcon,
+	QrCodeIcon,
+	RocketLaunchIcon,
 } from "@heroicons/react/20/solid";
 import manWithPhone from '../../Assets/manWithScooter/scooter-rental-1.JPG';
 import './About.scss'
@@ -11,20 +11,20 @@ const features = [
 	{
 		name: "Locate",
 		description:
-			"Using a simple and intuitive app on your smartphone, find a scooter nearby that's available for use. With real-time tracking, finding a scooter is as easy as checking your phone.",
-		icon: CloudArrowUpIcon,
+			"Using a simple app on your smartphone, find a scooter nearby. With real-time tracking, finding a scooter is as easy as checking your phone.",
+		icon: MapIcon,
 	},
 	{
 		name: "Scan",
 		description:
 			"Once you've found your scooter, unlocking it is a breeze. Simply open the app and scan the QR code on the scooter to unlock it and start your ride",
-		icon: LockClosedIcon,
+		icon: QrCodeIcon,
 	},
 	{
 		name: "Ride",
 		description:
-			"You're all set! Hop on the scooter and ride to your destination. Enjoy the freedom of mobility and the joy of a sustainable, convenient commute. When you're done, park the scooter responsibly and end your ride on the app. It's that simple!",
-		icon: ServerIcon,
+			"You're all set! Hop on the scooter and ride. Enjoy the freedom of mobility. When you're done, park the scooter responsibly and end your ride on the app. It's that simple!",
+		icon: RocketLaunchIcon,
 	},
 ];
 
@@ -34,20 +34,20 @@ export default function About() {
 			<div className="mx-auto max-w-7xl md:px-6 lg:px-8">
 				<div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
 					<div className="px-6 lg:px-0 lg:pr-4 lg:pt-4">
-						<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
-							<h2 className="text-base font-semibold leading-7 text-indigo-600">
+						<div className="text-container mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
+							<h2 className="get-started text-base font-semibold leading-7 text-indigo-600">
 								Get Started
 							</h2>
 							<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
 								How it Works?
 							</p>
 							<p className="mt-6 text-lg leading-8 text-gray-600"></p>
-							<dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+							<dl className="mt-10 max-w-xl space-y-20 text-base leading-7 text-gray-600 lg:max-w-none">
 								{features.map((feature) => (
 									<div key={feature.name} className="relative pl-9">
 										<dt className="inline font-semibold text-gray-900">
 											<feature.icon
-												className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+												className="icons absolute left-1 top-1 h-7 w-7"
 												aria-hidden="true"
 											/>
 											{feature.name}
@@ -70,7 +70,7 @@ export default function About() {
 									alt="Product screenshot"
 									width={2432}
 									height={1442}
-									className=" w-[57rem]  rounded-tl-xl bg-gray-800 ring-1 ring-white/10"
+									className=" image-man   rounded-tl-xl  ring-1 ring-white/10"
 								/>
 							</div>
 							<div
