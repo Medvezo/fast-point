1e63945b51ac79244bdd32b00edbf68404c6f88e
Medvezo <mansur.mustafayev@gmail.com>
2023-05-29 21:54:18 +0400

    First Raw Front-end Commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import Header from "../Header/Header";
+import Banner from "../Banner/Banner";
+import Footer from "../Footer/Footer";
+import Hero from "../Hero/Hero";
+import Bonus from "../Bonus/Bonus";
+import LogoCloud from "../LogoCloud/LogoCloud";
+import Pricing from "../Pricing/Pricing";
+import Stats from "../Stats/Stats";
+import About from "../About/About";
+import Support from "../Support/Support";
+import Contact from "../Contact/Contact";
+import FAQ from "../FAQ/FAQ";
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Banner />
+			<Hero />
+			<About />
+			<Stats />
+			<Bonus />
+			<FAQ />
+			<Pricing />
+			<Support />
+			<Contact />
+			<LogoCloud />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
