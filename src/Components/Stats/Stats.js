e17ddc5607828fd93bee4a61c50e9f2109d2188e
Medvezo <mansur.mustafayev@gmail.com>
2023-05-30 12:06:55 +0400

    Complete Stats

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Stats.scss'
 
 const stats = [
 	{ id: 1, name: "Number of Active Users", value: "2000+" },
 	{ id: 2, name: "Distance Traveled", value: "10000km+" },
 	{ id: 4, name: "New users monthly", value: "300+" },
-	{ id: 3, name: "CO2 Emissions Saved", value: "1.2 ton" },
+	{ id: 3, name: "CO2 Emissions Saved", value: "1.2 ton+" },
 
 ];
 
@@ -13,13 +14,13 @@ export default function Stats() {
 		<div className="bg-white py-24 sm:py-32">
 			<h2 className="text-5xl font-bold mb-52" >Statistics Tells the Truth!</h2>
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-4">
+				<dl className="text-container grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-4">
 					{stats.map((stat) => (
 						<div
 							key={stat.id}
 							className="mx-auto flex max-w-xs flex-col gap-y-4">
-							<dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
-							<dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+							<dt className="text-name font-semibold text-base leading-10">{stat.name}</dt>
+							<dd className="text-value order-first text-3xl font-semibold tracking-tight text-blue-900 sm:text-5xl">
 								{stat.value}
 							</dd>
 						</div>
