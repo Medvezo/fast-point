cdfa8f1d6f0d7ad8210af6641adde7bd571955d9
Medvezo <mansur.mustafayev@gmail.com>
2023-06-03 16:38:03 +0400

    Complete LogoCloud

--- src/Components/LogoCloud/LogoCloud.js
+++ src/Components/LogoCloud/LogoCloud.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import shopStore from "../../Assets/shopStore-noBG.png"
 export default function LogoCloud() {
 	return (
 		<div className="bg-gray-900 py-24 sm:py-32">
@@ -9,16 +9,17 @@ export default function LogoCloud() {
 				</h2>
 				<div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
 					
-					]
+					
 					<img
-						className="col-span-2 max-h-12 w-full object-contain sm:col-start-3 lg:col-span-5"
-						src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg"
+						className="max-h-20 w-full object-contain sm:col-start-3 lg:col-span-5"
+						src={shopStore}
 						alt="SavvyCal"
-						width={158}
-						height={48}
+						width={200}
+						height={200}
 					/>
 					
 				</div>
+					<p className="font-semibold text-yellow-600 mx-auto pt-5 text-2xl">ShopStore.az</p>
 			</div>
 		</div>
 	);
